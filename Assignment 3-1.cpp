89f4940c6747f796e5f54736cdc26ee691c6ec46
Sabahaiti <sxahebaier@zonemail.clpccd.edu>
2022-02-10 11:04:16 -0800

    submit 3-1

--- Assignment 3-1.cpp
+++ Assignment 3-1.cpp
@@ -3,5 +3,23 @@ using namespace std;
 
 int main()
 {
-    
+    int class A;
+    int class B;
+    int class C;
+
+    int price A = 15;
+    int price B =12;
+    int price C = 9;
+
+    cout << "Enter the number of the Tickets sold for class A: ";
+    cin >> class A;
+    cout << "Enter the number of the Tickets sold for class B: ";
+    cin >> class B;
+    cout << "Enter the number of the Tickets sold for class C: ";
+    cin >> class C;
+
+    total = (class A * price A)+(class B * price B)+(class C * price C);
+
+    cout << "Total price for ticket sold " << total < endl;
+
 }
