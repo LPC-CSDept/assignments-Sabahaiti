a7e0da26a6c6356299788b7036153d79ccdd2778
Sabahaiti <sxahebaier@zonemail.clpccd.edu>
2022-02-22 13:06:36 -0800

    Quiz3

---
+++ Quiz3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdlib> // heade file for the rand and srand
+#include <ctime> // time function, to give different seed umber at each run.
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    
+}
