cd874f6e13b21a1ce8fb6a033e4ac2e0de0b4261
Sabahaiti <sxahebaier@zonemail.clpccd.edu>
2022-02-22 13:16:31 -0800

    Quiz3

--- Quiz3.cpp
+++ Quiz3.cpp
@@ -13,14 +13,15 @@ int main()
     srand(time(0)); // random number time function
 
     // display the three random numbers
-    num1 = rand();
+    // If I set it as only rand() it takes a big number, so I decide it to make the numbers between 0~99
+    num1 = rand() % 100 ;
     cout << num1 << endl;
-    num2 = rand();
+    num2 = rand() % 100 ;
     cout << num2 << endl;
-    num3 = rand();
+    num3 = rand() % 100;
     cout << num3 << endl;
 
-    // display the sum of the random numbers
+    // display the sum of the three random numbers
     sum = num1 + num2 + num3;
     cout << "Total :" << sum << endl;
 
