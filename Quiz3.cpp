e6d0e794deb591d615ac146b3dd7a3d706721295
Sabahaiti <sxahebaier@zonemail.clpccd.edu>
2022-02-22 13:13:45 -0800

    Quiz3

--- Quiz3.cpp
+++ Quiz3.cpp
@@ -13,5 +13,20 @@ int main()
     srand(time(0)); // random number time function
 
     // display the three random numbers
-    
+    num1 = rand();
+    cout << num1 << endl;
+    num2 = rand();
+    cout << num2 << endl;
+    num3 = rand();
+    cout << num3 << endl;
+
+    // display the sum of the random numbers
+    sum = num1 + num2 + num3;
+    cout << "Total :" << sum << endl;
+
+    // display the average of the three random numbers
+    avg = sum / 3.0;
+    cout << "Average :" << avg << endl;
+
+    return 0;
 }
