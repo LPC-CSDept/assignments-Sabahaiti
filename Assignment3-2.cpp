6baf60b2ab03f41daa071fedff5e42c300c6250a
Sabahaiti <sxahebaier@zonemail.clpccd.edu>
2022-02-27 17:58:44 -0800

    Assignment3-2

--- Assignment3-2.cpp
+++ Assignment3-2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
@@ -6,20 +7,24 @@ int main()
     int nummales;
     int numfemales;
     int total; 
+    int percent;
 
-    double males;
-    double females;
+    double malepercent;
+    double femalepercent;
+
 
     cout << "Enter the nummales: ";
     cin >> nummales;
     cout << "Enter the numfemales: ";
     cin >> numfemales;
     
-    total = (nummales + numfemales);
+    total = nummales + numfemales;
+    cout << total << endl;
    
-    males = (nummales / total * 100);
-    females = (numfemales / total * 100);
-
-    cout << males  <<  females << endl;
+    malepercent = (nummales / total * 0.1);
+    cout << nummales <<endl ;
+    femalepercent = (numfemales / total * 0.1);
+    cout << numfemales << endl;
+    cout << nummales  <<  numfemales << endl;
     return 0;
 }
