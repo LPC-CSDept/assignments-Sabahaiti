31a2603e9b46025378dbe1a51aa513f67e95f6c3
Sabahaiti <sxahebaier@zonemail.clpccd.edu>
2022-03-02 18:00:29 -0800

    quiz41

---
+++ quiz41.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    // declear the caraible
+    int menu;
+    double area;
+    double radius;
+    double length;
+    double width;
+    double base;
+    double heigth;
+
+    double π = 3.14159; //for the circle equation pi
+
+    const int Circle_menu = 1;
+    const int Rectangle_menu = 2;
+    const int Triangle_menu = 3;
+    const int Quit_menu = 4;
+
+    // print the menu
+    cout << "Menu \n";
+    cout <<"Area of Circle \n";
+    cout << "Area of Rectangle \n ";
+    cout <<" Area pf Triangle \n";
+    cout <<"Quit \n";
+    cin >> menu;
+
+    
+
+
+
+
+
+
+
+
+
+}
