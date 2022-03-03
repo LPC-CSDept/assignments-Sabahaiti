1f82945b8369e40ab21aa69a4cf6821a1cb0fe52
Sabahaiti <sxahebaier@zonemail.clpccd.edu>
2022-03-02 18:16:21 -0800

    quiz4

--- quiz41.cpp
+++ quiz41.cpp
@@ -28,7 +28,38 @@ int main()
     cout <<"Quit \n";
     cin >> menu;
 
-    
+    switch(menu)
+       {
+           // to find the area of the circle
+           case 1:
+           cout <<"radius of the circle : \n";
+           cin >> radius;
+         
+        if (radius < 0)
+          {
+             cout << "Enter the radius one more time : \n" ;
+
+          }
+        area = π * radius * radius;
+        cout <<" Circle are is equal to " << area <<endl;
+        break;
+
+        // for the rectangle
+            case 2:
+            cout << "length of the rectangle : \n";
+            cin >> length;
+
+        if (length < 0)
+            {
+                cout << "Enter length one more time : \n";
+                cin >> length;
+            }
+
+
+
+       }
+
+
 
 
 
