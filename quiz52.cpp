66e4dd74e952adc1f8138088fcd58d206e3cde9d
Sabahaiti <sxahebaier@zonemail.clpccd.edu>
2022-03-22 15:04:28 -0700

    quiz52

--- quiz52.cpp
+++ quiz52.cpp
@@ -3,5 +3,14 @@ using namespace std;
 // make program that shows the N to the power of 0, 1,2,3,...M
 int main()
 {
+    int N, M;
+
+    cout << "Enter you input:";
+    cin >> N;
+    M =1;
+    while (M <=N){
+        cout << M << "\t"<< N* M << endl;
+        M +=1;
+    }
 
 }
