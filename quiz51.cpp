6a9914a3ed1acffad8014f57d4f2c5c4c11d090a
Sabahaiti <sxahebaier@zonemail.clpccd.edu>
2022-03-22 14:11:50 -0700

    quiz51

--- quiz51.cpp
+++ quiz51.cpp
@@ -2,8 +2,24 @@
 #include <cstdlib>
 #include <ctime>
 using namespace std;
-
+// minimum 5 random numbers
+//The range of random numbers 0-99
 int main()
 {
+    const int N=5;
+    int rdnum;
+    int min;
+   
 
+    srand(time(0));
+
+    for (int i=0; i < 5; i++){
+        rdnum = rand()%100;
+        cout << rdnum<< endl;
+
+        if ((i==0)|| (min > rdnum))
+            min = rdnum;
+
+    }
+    cout << "Min:" <<  min<< endl;
 }
