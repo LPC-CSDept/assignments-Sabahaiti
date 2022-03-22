1bff3e2a9cea51336b337a02de6985ecb9fca728
Sabahaiti <sxahebaier@zonemail.clpccd.edu>
2022-03-22 15:43:39 -0700

    submit

--- quiz54read.cpp
+++ quiz54read.cpp
@@ -5,5 +5,11 @@ using namespace std;
 int main()
 {
     ifstream ifs;
+    int Employee_ID;
+    string Employee_name;
+    string Department_name;
+    double salary;
+    double sum, avg;
+    double tsum, tavg;
     
 }
