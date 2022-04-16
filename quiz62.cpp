2fc59abecba769ab683b7da43fc7efc79688fd93
Sabahaiti <sxahebaier@zonemail.clpccd.edu>
2022-04-15 21:04:56 -0700

    Chapter 6 quiz 2

---
+++ quiz62.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+//The program generates three random numbers and 
+//then find the min number among these values. 
+//We need to develop three functions as follows:
+
+void getRandom(int &n1, int &n2, int &n3);
+int findMin(int n1,int n2, int n3);
+void printResult(int n1, int n2, int n3, int min);
+
+int main()
+{
+    int n1, n2, n3;
+    int min=findMin(n1, n2, n3);
+    int printResult(n1, n2, n3, min);
+
+}
