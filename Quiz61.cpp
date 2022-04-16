d5083ff24fa73bd3f469b24eeb40f2d149b94bc0
Sabahaiti <sxahebaier@zonemail.clpccd.edu>
2022-04-15 20:27:58 -0700

    quiz chapter 6

---
+++ Quiz61.cpp
@@ -0,0 +1,9 @@
+//Make a program that calls the two overloaded functions swapTwoValues( ) 
+//so that two values are swapped in the function. According to the different 
+//parameter data types, one of the two functions will be invoked.
+
+//void swapTwoValues(int n1, int n2) :  Two integer values are swapped
+//void swapTwoValues(float f1, float f2) : Two float values are swapped
+
+#include <iostream>
+using namespace std;
