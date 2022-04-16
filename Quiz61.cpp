08292ac60168613d39c12db20f9589ae8b73ed6b
Sabahaiti <sxahebaier@zonemail.clpccd.edu>
2022-04-15 20:43:52 -0700

    chapter 6

--- Quiz61.cpp
+++ Quiz61.cpp
@@ -6,4 +6,25 @@
 //void swapTwoValues(float f1, float f2) : Two float values are swapped
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
+//function prototype, the different parameter 
+
+void swapTwoValues(int n1, int n2);
+void swapTwoValues(float f1, float f2);
+
+//call
+
+int main()
+{
+    int n1, n2;
+    float f1, f2;
+
+    cout << fixed << showpoint << setprecision(2);
+    cout <<"Enter an integer :";
+    cout <<"Enter a floating value: ";
+    cin >> n1 >> "" >> n2 >>"" >>f1 >>"" >> f2;
+    
+
+}
