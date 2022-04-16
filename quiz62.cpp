bbeb0224816b51cf4f6a372b40fa75302476c5a2
Sabahaiti <sxahebaier@zonemail.clpccd.edu>
2022-04-16 13:44:06 -0700

    submit

--- quiz62.cpp
+++ quiz62.cpp
@@ -8,21 +8,17 @@ using namespace std;
 //We need to develop three functions as follows:
 
 void getRandom(int &n1, int &n2, int &n3);
-int findMin(int n1,int n2, int n3);
-void printResult(int n1, int n2, int n3, int min);
-
-int main()
 {
-    int n1, n2, n3;
-    int min=findMin(n1, n2, n3);
-    int Result = n1, n2, n3, min;
-    
-    int rdnum;
-    srand(time(0));
-    
-    void getRandom(int &n1, int &n2, int &n3)
-    {
+    n1= rand();
+    n2=rand();
+    n3=rand();
+}
+int findMin(int n1, int n2, int n3)
+{
+    if (n1 < n2 && n1 < n3)
+    return n1;
+    if(n2<n3 && n2< n1)
+    return n2;
 
-    }
-    
+    return n3;
 }
