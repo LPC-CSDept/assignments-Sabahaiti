919c703b844cb5a4fb7ae1a5587a8cd0613f1ff2
Sabahaiti <sxahebaier@zonemail.clpccd.edu>
2022-04-17 10:36:22 -0700

    submit

--- Quiz61.cpp
+++ Quiz61.cpp
@@ -11,20 +11,44 @@ using namespace std;
 
 //function prototype, the different parameter 
 
-void swapTwoValues(int n1, int n2);
-void swapTwoValues(float f1, float f2);
+void swapTwoValues(int &n1, int &n2);
+void swapTwoValues(float &f1, float &f2 );
 
 //call
 
 int main()
 {
-    int n1, n2;
+    int n1, n2 ;
     float f1, f2;
-
-    cout << fixed << showpoint << setprecision(2);
-    cout <<"Enter an integer :";
-    cout <<"Enter a floating value: ";
-    cin >> n1 >> "" >> n2 >>"" >>f1 >>"" >> f2;
     
+    cout <<"Enter two integers:";
+    cin >> n1 >> n2;
 
+    cout <<"ENter two floating:";
+    cin >> f1>> f2;
+
+    swapTwoValues(n1, n2);
+    cout <<"\nAfter swapping";
+    cout <<"\n n1=" << n1 << "\nn2="<< n2;
+    cout <<"\nfloating";
+    cout <<"\nf1="<<f1 << "\nf2=" <<f2;
+    swapTwoValues(f1,f2);
+    cout <<"\nAfter swapping";
+    cout <<"\nf1=" <<f1 <<"\nf2=" <<f2;
+    return 0;
+
+}
+void swap(int &n1, int &n2)
+{
+    int temp;
+    temp =n1;
+    n1= n2;
+    n2=temp;
+}
+void swap(float &f1, float &f2)
+{
+    float temp;
+    temp =f1;
+    f1= f2;
+    f2=temp;
 }
