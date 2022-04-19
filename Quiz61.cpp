18ea1585b7b19250b048493ba710e973fda906ae
Sabahaiti <sxahebaier@zonemail.clpccd.edu>
2022-04-19 11:01:00 -0700

    submit

--- Quiz61.cpp
+++ Quiz61.cpp
@@ -38,14 +38,14 @@ int main()
     return 0;
 
 }
-void swap(int &n1, int &n2)
+void swapTwoValues(int &n1, int &n2)
 {
     int temp;
     temp =n1;
     n1= n2;
     n2=temp;
 }
-void swap(float &f1, float &f2)
+void swapTwoValues(float &f1, float &f2)
 {
     float temp;
     temp =f1;
