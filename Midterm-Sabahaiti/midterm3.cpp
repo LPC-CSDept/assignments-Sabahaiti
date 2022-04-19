ba514ebf5c3682475a6640238edca2fd6d5fa761
Sabahaiti <sxahebaier@zonemail.clpccd.edu>
2022-04-19 16:37:18 -0700

    submit

---
+++ Midterm-Sabahaiti/midterm3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+using namespace std;
+
+// N defined as 10
+
+int number=51;
+
+int getRdnum(void);
+{
+    return rand()% 50 +1;
+}
