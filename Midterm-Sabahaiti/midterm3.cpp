a8ec58249e1a8ac0623ebaba1bc386e9527df2ee
Kyu Lee <kwooler@gmail.com>
2022-04-22 19:40:27 -0700

    midterm

--- Midterm-Sabahaiti/midterm3.cpp
+++ Midterm-Sabahaiti/midterm3.cpp
@@ -6,44 +6,49 @@ using namespace std;
 
 // N defined as 10
 
-int number=51;
+int number = 51;
 
-int getRdnum(void);
+// int getRdnum(void);
+int getRdnum(void)
 {
-    return rand()% 50 +1;
+    return rand() % 50 + 1;
 }
 
-int isGreater(int n);
+// int isGreater(int n);
+int isGreater(int n)
 {
+    static int num = 0;
 
-    if (n> num)
+    if (n > num)
     {
-        num=n;
+        num = n;
         return 1;
-    } else
+    }
+    else
     {
-        num =n;
+        num = n;
         return 0;
     }
-    int main()
+    // Need closing braces
+}
+
+int main()
 {
-    int N=10;
+    int N = 10;
     ofstream ofs;
 
-    ofs.open ("number.txt");
-    if(!ofs)
+    ofs.open("number.txt");
+    if (!ofs)
     {
-        cerr <<"File open Error\n";
+        cerr << "File open Error\n";
         exit(0);
-
     }
-    for(int i=0; i<N;i++)
+    for (int i = 0; i < N; i++)
     {
-        int num= getRdnum();
-        cout <<num <<" ";
-        if(isGreater(num))
-        ofs <<num<<" ";
-
+        int num = getRdnum();
+        cout << num << " ";
+        if (isGreater(num))
+            ofs << num << " ";
     }
     ofs.close();
 }
