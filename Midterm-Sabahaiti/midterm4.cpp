e7eb79ce907ca1e5ef382b4565dfd760d096f208
Sabahaiti <sxahebaier@zonemail.clpccd.edu>
2022-04-26 15:51:07 -0700

    submit

--- Midterm-Sabahaiti/midterm4.cpp
+++ Midterm-Sabahaiti/midterm4.cpp
@@ -10,3 +10,18 @@ int getRdnum()
     srand(time(0));
     return rand()% 100+1;
 }
+
+//getRdnum(n2), with a parameter, it will return a random number between 1 to n2
+int getRdnum(int n2)
+{
+    srand(time(0));
+    return rand()% n2+1;
+}
+
+//getRdnum(n1, n2), it will return a random number between n1+1 to n2
+int getRdnum(int n1, int n2)
+{
+    srand(time(0));
+    return rand()% (n2-(n1+1)+1)+n1+1;
+}
+
