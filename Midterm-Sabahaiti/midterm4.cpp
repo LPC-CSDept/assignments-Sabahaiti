b9e6517c7e0c6adb71102c8c7193f6195c620ac1
Sabahaiti <sxahebaier@zonemail.clpccd.edu>
2022-04-26 15:44:07 -0700

    submit

--- Midterm-Sabahaiti/midterm4.cpp
+++ Midterm-Sabahaiti/midterm4.cpp
@@ -3,3 +3,10 @@
 #include <ctime>
 using namespace std;
 
+
+//getRdnum( ), without parameter, it will return a random number between 1 to 100
+int getRdnum()
+{
+    srand(time(0));
+    return rand()% 100+1;
+}
