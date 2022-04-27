625f38198ae138b6da6a3f8e688dfaae8520b418
Sabahaiti <sxahebaier@zonemail.clpccd.edu>
2022-04-26 20:51:20 -0700

    submit

--- Midterm-Sabahaiti/midterm5.cpp
+++ Midterm-Sabahaiti/midterm5.cpp
@@ -2,3 +2,64 @@
 #include <fstream>
 using namespace std;
 
+void getInput(int &, int &);
+int isSame(int, int);
+int intDivision(int, int);
+void fileWrite(ofstream &, int);
+
+
+int main()
+{
+    int num1, num2;
+    int result;
+    ofstream ofs;
+
+    getInput(num1, num2);
+    
+
+    if (num1 >num2)
+    {
+        result = intDivision(num1, num2);
+
+    }
+    else 
+    {
+        result = intDivision(num2, num1);
+    }
+    fileWrite(result);
+
+}
+void getInput(int &num1, int &num2)
+{
+    cin >> num1 >> num2;
+    
+}
+
+int isSame(int &num1, int &num2)
+{
+    if(num1 == num2)
+    {
+        return 1;
+    }
+}
+
+int intDivision(int &num1, int &num2)
+{
+    if(num1 > num2)
+    {
+        return num1 / num2;
+    }
+    else
+    {
+        return num2 / num1;
+    }
+}
+
+void fileWrite (ofstream & ofs, int result)
+{
+    ofstream fileWrite;
+    fileWrite.open("question5.txt");
+    fileWrite << result << "\n";
+    fileWrite.close();
+
+}
