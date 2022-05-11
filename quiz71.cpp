9517b078fdd8bef85432a495f1449906560e3d83
Sabahaiti <sxahebaier@zonemail.clpccd.edu>
2022-05-10 21:33:59 -0700

    Quiz7

--- quiz71.cpp
+++ quiz71.cpp
@@ -10,13 +10,25 @@ int main()
 
     for(int i=0; i< N; i++)
     {
-        if ( abs (n[i] - n[i+1]) < 10)
+        if i==0 && i+1 <N)
         {
-            cout << number [i] << endl;
-
+            if ( abs (n[i] - n[i+1]) < 10) // find the first difference of 10
+            {
+                cout << n[i] <<" less than 10 \n ";
+            }
+        }else (i==N-1 && i> 0)
+        {
+              if (abs (n[i] - n[i-1]) < 10) // find last difference of 10
+            {
+                cout<< n[i] <, endl;
+            }
+        }else if (( abs ( n[i] - n[i+1]) <10) && (abs (n[i]-n[i-1])<10)
+        {
+            cout << n[i] << endl;
         }
-
-    }   else 
+          
+    }   
+   
 
 
 }
