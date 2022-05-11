6dbc60ee2f2fabd1c38d5893fbb040180852e798
Sabahaiti <sxahebaier@zonemail.clpccd.edu>
2022-05-10 21:13:08 -0700

    Quiz 7

--- quiz71.cpp
+++ quiz71.cpp
@@ -6,5 +6,17 @@ const int N =10;
 
 int main()
 {
+    int number [N] = {10, 15,0,-25,19,20,25,30,45,20}; // initializing the array
+
+    for(int i=0; i< N; i++)
+    {
+        if ( abs (n[i] - n[i+1]) < 10)
+        {
+            cout << number [i] << endl;
+
+        }
+
+    }   else 
+
 
 }
