9fbfb71ed88ecdce6376951613ca704b188fe5fc
Sabahaiti <sxahebaier@zonemail.clpccd.edu>
2022-05-10 23:12:41 -0700

    Quiz7

--- quiz74.cpp
+++ quiz74.cpp
@@ -1,7 +1,45 @@
 #include <iostrema>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <algorithm>
+#include <iterator>
 using namespace std;
+
+void makevector(vector <int> &, int);
+void printvector(vector <int>);
+void deleteone (vector <int> &);
 
 int main()
 {
-    
+    vector <int> number;
+    int size=20;
+    srand(time(0));
+    makevector(number,size);
+    printvector(number);
+    deleteone(number);
+
+}
+void makevector(vector <int> &vec, int size)
+{
+    for (int i=0; i<size; i++)
+        vec.push_back(rand()%10);
+
+}
+void printvector(vector<int> vec)
+{
+    for(int v:vec)
+        cout <,v<<"\t";
+    cout <<endl;
+}
+void deleteone(vector <int> &vec)
+{
+    int usernum, deletecnt=0;
+    cin >> usernum;
+
+    // didn't finished yet, I still need work on it 
+
+    cout << usernum<<"is deleted " <<deletecnt << "times\n";
+
+    printvector(vec)
 }
