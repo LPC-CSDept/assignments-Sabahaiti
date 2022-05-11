8bb3be8a9aab957e628b1e98d8a08ecbc022f4c5
Sabahaiti <sxahebaier@zonemail.clpccd.edu>
2022-05-10 21:59:56 -0700

    Quiz7

--- quiz72.cpp
+++ quiz72.cpp
@@ -1,7 +1,19 @@
 #include <iostrema>
 using namespace std;
 
+void printtriangle(int numbers [][3]. int)
 int main()
+{
+    const int SIZE = 3;
+
+    int numbers[SIZE][SIZE] = { {0,1,2},
+                            {3,4,5},
+                            {6,7,8}};
+    
+    printtriangle(numbers, SIZE);
+}
+
+void printtriangle(int num[][3], int size)
 {
     
 }
