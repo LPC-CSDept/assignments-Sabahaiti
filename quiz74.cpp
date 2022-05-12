4581feb374da8dab1fec30622c5e2a4d6d27fc55
Sabahaiti <sxahebaier@zonemail.clpccd.edu>
2022-05-11 21:44:44 -0700

    Quiz7

--- quiz74.cpp
+++ quiz74.cpp
@@ -1,4 +1,4 @@
-#include <iostrema>
+#include <iostream>
 #include <cstdlib>
 #include <ctime>
 #include <vector>
@@ -23,23 +23,32 @@ int main()
 void makevector(vector <int> &vec, int size)
 {
     for (int i=0; i<size; i++)
-        vec.push_back(rand()%10);
+        vec. push_back(rand()%10);
 
 }
 void printvector(vector<int> vec)
 {
-    for(int v:vec)
-        cout <,v<<"\t";
+    for(int v : vec)
+          cout <<v<<"\t";
     cout <<endl;
 }
 void deleteone(vector <int> &vec)
 {
-    int usernum, deletecnt=0;
+    int usernum;
     cin >> usernum;
 
-    // didn't finished yet, I still need work on it 
+    vector<int>::iterator it;
+
+    it=std::find(v.begin(), v.end(),usernum);
+    int deleteone=0;
+}
+while(it=v.begin())
+{
+    v.erase(it);
+    deleteone++;
+        it=std::find(v.begin(), v.end(), usernum);
+}
 
     cout << usernum<<"is deleted " <<deletecnt << "times\n";
 
-    printvector(vec)
-}
+    printvector(vec);
