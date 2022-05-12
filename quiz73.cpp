7e188500e5bc0127ce1d4f55ae9ba6d12d777f73
Sabahaiti <sxahebaier@zonemail.clpccd.edu>
2022-05-11 21:50:33 -0700

    Quiz7

--- quiz73.cpp
+++ quiz73.cpp
@@ -5,12 +5,17 @@ int findmax(int number[], int from, int to);
 int main()
 {
     const int N=10;
-    int max1, max2;
+    int max1, max2,i;
     int number [N] = {10, 15,0,-25,19,20,25,30,45,20};
 
-    max1=findmax(number, 0, N/2-1);
+    for(int i=0; i<N, i++)
+    {
+       
+    }
+
+    max1=findmax(number, 0, N/2-1);//first half
     cout << max1<< endl;
-    max2=findmax(number, N/2, N-1);
+    max2=findmax(number, N/2, N-1);//second half
     cout << max2 << endl;
   
 }
