570961d7c50a7f051304abe1b0115eb57af98714
Sabahaiti <sxahebaier@zonemail.clpccd.edu>
2022-05-19 20:39:28 -0700

    Lab11

--- Assignment5-2.cpp
+++ Assignment5-2.cpp
@@ -3,5 +3,24 @@ using namespace std;
 
 int main()
 {
+    int begin;
+    int end;
 
+    int i;
+    int j;
+
+    cin >> begin >> end;
+
+    while (begin >= end || begin <2 || end <0);
+
+    for(i=begin; i<=end; i++)
+    {
+        if(i==0 || i==1)
+        
+        for(j=2; j<=(i/2); j++)
+            if(i% j ==0)
+                break;
+        if (j>(i/2))
+            cout << i << endl;
+    }
 }
