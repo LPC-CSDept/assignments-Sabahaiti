532df7543504309f25ccfc2e566e87dbd77821a0
Sabahaiti <sxahebaier@zonemail.clpccd.edu>
2022-05-26 09:28:03 -0700

    q3

--- Final/q3.cpp
+++ Final/q3.cpp
@@ -7,4 +7,25 @@ int main()
     const int SIZE2 =10;
     int numbers1[SIZE1] ={2, 3, 5, 9, 10};
     int numbers2 [SIZE2] = {1,2,3,5,9,10,11,13,15,17};
+
+    unordered_set<int> s;
+    for (int i=0; i<SIZE1; i++)
+    {
+        s.insert(numbers1[i]);
+    }
+    int p= s.size();
+    for (int i=0; i< SIZE2; i++)
+    {
+        s.insert(numbers2[i]);
+
+    }
+    if (s.size() == p)
+    {
+        cout << "number1 is subset of number 2" << endl;
+
+    }
+    else 
+    {
+        cout << "number 1 is not subset of number 2" << endl;
+    }
 }
