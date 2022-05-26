a9a6d53955047effbf3940b6314f7b1e2faa648e
Sabahaiti <sxahebaier@zonemail.clpccd.edu>
2022-05-26 09:50:59 -0700

    q4

--- Final/q4.cpp
+++ Final/q4.cpp
@@ -21,8 +21,14 @@ Grade g;
 
 int main()
 {
+    Grade g;
+    string field;
     string buf;
     stringstream ss;
+    int i=0; 
+    int fcnt=0;
+
+
 
     ifstream ifso;
     ifso. open ("grades.txt", fstream::in);
@@ -34,8 +40,34 @@ int main()
         cout << "open Error\n";
         exit(0);
     }
+    while (ifso >> buf)
+    {
+        ss<< buf;
+        while (getline (ss,field, ','))
+        {
+            switch(fcnt)
+            {
+                case 0:
+                    g.first =field;
+                    break;
+                case 1;
+                    g.last =field;
+                    break;
+                case 2:
+                    g.ssn = field;
+                case 3:
+                    g.score= field;
+                    break;
+                    
+            }
+            fcnt =++fcnt %5;
+        }
+        ss.clear();
+        ofso.write((char*) &g,sizeof(g));
 
-    
+        
+    }
+
 
 
 }
