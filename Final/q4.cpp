5cb12b5500af6af35915f68ce3aea33b63d134cb
Sabahaiti <sxahebaier@zonemail.clpccd.edu>
2022-05-26 08:43:04 -0700

    q4

--- Final/q4.cpp
+++ Final/q4.cpp
@@ -5,5 +5,28 @@
 #include <iomanip>
 using namespace std;
 
+const unsigned short N=5;
+struct Scores{
+    double sc[N];
+    string grade;
+};
+
+struct Grade{
+    string first;
+    string last;
+    string ssn;
+    Scores score;
+};
+Grade g;
+
+int main()
+{
+    ifstream ifso;
+    ifso. open ("grades.txt", fstream::in);
+    ofstream ofso;
+    ofso.open ("grades.bin", fstream::out);
+    
+
+}
 
 
