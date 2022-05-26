7859d5fdaac901401d6178e40dc1477acfe3728b
Sabahaiti <sxahebaier@zonemail.clpccd.edu>
2022-05-26 15:57:45 -0700

    another way of q3

--- Final/question3.cpp
+++ Final/question3.cpp
@@ -1,3 +1,15 @@
 #include <iostream>
 using namespace std;
 
+bppl isSubset(int numbers1[], int numbers2[], int SIZE1, int SIZE2)
+{
+    int i=0;
+    int j=0;
+    for(i=0; i<SIZE1; i++)
+    {
+        for(j=0; j<SIZE2; j++)
+        {
+            if(numbers1[i] == numbers2[j])
+        }
+    }
+}
